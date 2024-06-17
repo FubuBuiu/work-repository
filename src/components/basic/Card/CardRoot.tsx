9709797ed1d07902cd4957880f68a06b161267c5
EMDAGRO\brendon.rodrigues <brendon.rodrigues@sergipetec.org.br>
2024-06-17 10:00:37 -0300

    chore(component): :twisted_rightwards_arrows: Update components from Boilerplate

--- src/components/basic/Card/CardRoot.tsx
+++ src/components/basic/Card/CardRoot.tsx
@@ -6,10 +6,8 @@ interface CardRootPropsType extends HTMLAttributes<HTMLDivElement> {
 
 export default function CardRoot({ children, className, glass, ...props }: CardRootPropsType) {
     return (
-        <div className='w-fit'>
-            <div className={`card gap-2 shadow-md  ${glass ? 'glass' : 'bg-base-100'} ${className} ${className?.includes('card-compact') ? 'p-4' : 'p-8'}`} {...props}>
-                {children}
-            </div>
+        <div className={`card gap-2 shadow-md  ${glass ? 'glass' : 'bg-base-100'} ${className} ${className?.includes('card-compact') ? 'p-4' : 'p-8'}`} {...props}>
+            {children}
         </div>
     );
 }
